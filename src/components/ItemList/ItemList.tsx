d55f74b64fcd92f532c6919601b558d500d9a1b9
Micah Ives <micbenives@gmail.com>
2024-09-30 16:05:56 -0500

    Refactor add/edit/delete list items, separate components for simplicity

---
+++ src/components/ItemList/ItemList.tsx
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+
+export interface ItemType {
+    id: string;
+    title: string;
+}
+
+export interface ItemListProps {
+    items: ItemType[];
+    onUpdate: (item: ItemType) => void;
+    onDelete: (id: string) => void;
+}
+
+export function ItemList({ items, onUpdate, onDelete }: ItemListProps): JSX.Element {
+    return (
+        <ul>
+            {items.map(item => (
+                <li key={item.id}>
+                    <Item
+                        item={item}
+                        onUpdate={onUpdate}
+                        onDelete={onDelete}
+                    />
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+export interface ItemProps {
+    item: ItemType;
+    onUpdate: (item: ItemType) => void;
+    onDelete: (id: string) => void;
+}
+
+function Item({ item, onUpdate, onDelete }: ItemProps): JSX.Element {
+    const [isEditing, setIsEditing] = useState(false);
+    let itemContent;
+    if (isEditing) {
+        itemContent = (
+            <>
+                <input
+                    value={item.title}
+                    onChange={e => {
+                        onUpdate({
+                            ...item,
+                            title: e.target.value
+                        });
+                    }}
+                />
+                <button type='button' onClick={() => setIsEditing(false)}>
+                    Save
+                </button>
+            </>
+        );
+    } else {
+        itemContent = (
+            <>
+                {item.title}
+                <button type='button' onClick={() => setIsEditing(true)}>
+                    Edit
+                </button>
+            </>
+        );
+    }
+
+    return (
+        <label>
+            {itemContent}
+            <button type='button' onClick={() => onDelete(item.id)}>
+                Delete
+            </button>
+        </label>
+    )
+}
--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,23 +1,11 @@
 import { Welcome } from '../components/Welcome/Welcome';
-import { InputWithButton } from '@/components/InputWithButton/InputWithButton';
-import { Flex } from '@mantine/core';
+import { OverallList } from '@/components/OverallList/OverallList';
 
 export function HomePage() {
   return (
     <>
       <Welcome />
-
-      <Flex
-        mih={50}
-        gap="md"
-        justify="flex-start"
-        align="center"
-        direction="column"
-        wrap="wrap"
-        >
-        <InputWithButton/>
-      </Flex>
-
+      <OverallList />
     </>
   );
 }
