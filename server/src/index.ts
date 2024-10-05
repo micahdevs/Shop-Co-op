8533baf8a307744f8135508fe98db6e4acaf34d5
Micah Ives <micbenives@gmail.com>
2024-10-05 08:39:50 -0500

    Add resolvers for apollo server; cast Express app as type Application from apollo-server-express @types within modules to fix ts error

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,22 +1,37 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { List } from "./entities/List";
+// import { List } from "./entities/List";
 import mikroConfig from "./mikro-orm.config";
+import express from 'express';
+import {ApolloServer} from 'apollo-server-express'
+import {buildSchema} from 'type-graphql';
+import { HelloResolver } from "./resolvers/hello";
+import { Application } from "../node_modules/apollo-server-express/node_modules/@types/express/index"
 
 const main = async () => {
     const orm = await MikroORM.init(mikroConfig);
     await orm.getMigrator().up();
-
-    const emFork = orm.em.fork(); 
     // fork the entity manager to avoid directly using global EntityManager (em) instance
     // additional option to use the RequestContext helper:
     // (https://mikro-orm.io/docs/identity-map#why-is-request-context-needed)
 
-    const list = emFork.create(List, {title: 'Shopping List'} as List);
-    await emFork.persistAndFlush(list);
+    const app = express() as Application;
 
-    const lists = await emFork.find(List, {});
-    console.log(lists);
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        })
+    });
+
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(4000, () => {
+        console.log('server started on localhost:4000')
+    })
+
+
 };
 
 main().catch((err) => {
