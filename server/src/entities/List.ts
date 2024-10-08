fbfb0b200133ba5d192d5dac7e1b9204f725b634
Micah Ives <micbenives@gmail.com>
2024-10-07 20:46:58 -0500

    Update created/updatedAt type to be auto from MikroORM, add resolvers for single list and createList mutation

--- server/src/entities/List.ts
+++ server/src/entities/List.ts
@@ -1,20 +1,20 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
-import { ObjectType, Field } from "type-graphql";
+import { ObjectType, Field, ID } from "type-graphql";
 
 @ObjectType()
 @Entity()
 export class List {
-  @Field()
+  @Field(() => ID)
   @PrimaryKey()
   _id!: number;
 
-  @Field(() => String)
-  @Property()
-  createdAt = new Date();
+  @Field(() => String, { nullable: true }) // { nullable: true } makes it optional, plus "?" at end of field name
+  @Property( { onCreate: () => new Date })
+  createdAt?: Date = new Date();
 
-  @Field(() => String)
-  @Property({ onUpdate: () => new Date() })
-  updatedAt = new Date();
+  @Field(() => String, { nullable: true }) // MikroORM will then automatically handle the createdAt/updatedAt
+  @Property({ onUpdate: () => new Date()} )
+  updatedAt?: Date = new Date();
 
   @Field()
   @Property({type: 'text'})
--- server/src/resolvers/list.ts
+++ server/src/resolvers/list.ts
@@ -1,11 +1,29 @@
 import { List } from '../entities/List';
-import { Query, Resolver, Ctx } from 'type-graphql';
+import { Query, Resolver, Ctx, Arg, Int, Mutation } from 'type-graphql';
 import { MyContext } from '../types';
 
 @Resolver()
-export class ListResolver {
+export class ListResolver { 
     @Query(() => [List])
     lists(@Ctx() { em }: MyContext): Promise<List[]> {
         return em.find(List, {});
+    }    
+
+    @Query(() => List, { nullable: true })
+    list(
+        @Arg('id', () => Int, { nullable: true }) _id: number,
+        @Ctx() { em }: MyContext
+    ): Promise<List | null> {
+        return em.findOne(List, { _id });
+    }
+
+    @Mutation(() => List)
+    async createList(
+        @Arg('title', () => String) title: string,
+        @Ctx() { em }: MyContext
+    ): Promise<List> {
+        const list = em.create(List, { title });
+        await em.persistAndFlush(list);
+        return list;
     }
 }
